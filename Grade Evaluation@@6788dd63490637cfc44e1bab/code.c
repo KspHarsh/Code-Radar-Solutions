8075c6a11c8a5b655a30435e5c79b6798be162f1
KspHarsh <harsh1314.becse24@chitkara.edu.in>
2025-01-22 20:31:01 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() {
+    char grade:
+    scanf("%c",&grade);
+    if (grade='A'){
+        printf("Excellent");
+    }else if (grade='B'){
+        printf("Average");
+    }else if (grade='C'){
+        printf("Below Average");
+    }else if (grade='D'){
+        printf("Fail");
+    }else{
+        printf("Invalid grade");
+    }
+   
+    return 0;
+}
