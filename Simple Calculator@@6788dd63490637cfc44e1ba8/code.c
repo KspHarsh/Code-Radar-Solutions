016d2a4edd8128839e39186a167d668b70aeab69
KspHarsh <harsh1314.becse24@chitkara.edu.in>
2025-02-06 19:53:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -13,9 +13,12 @@ int main() {
         d=a+b;
         printf("%d",d);
     }else if(c== '/'){
-        d=a/b;
-        printf("%d",d);
-    }else {
+        if(b==0){
+            printf("error");
+        }else{
+            d=a/b;
+            printf("%d",d);
+        }else {
         printf("error");
     }
 
