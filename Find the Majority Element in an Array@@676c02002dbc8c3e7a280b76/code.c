64be4efed03487d7df798b2ba6904297d6d68ba4
Harsh <harshksp07@gmail.com>
2025-03-18 20:37:26 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,33 +1,54 @@
-// Your code here...
+// // Your code here...
+// #include <stdio.h>
+
+// int main() {
+//     int N;
+//     scanf("%d", &N);
+
+//     int arr[N];
+//     for (int i = 0; i < N; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+
+//     int candidate = arr[0], count = 1;
+
+//     for (int i = 1; i < N; i++) {
+//         if (arr[i] == candidate) count++;
+//         else count--;
+//         if (count == 0) {
+//             candidate = arr[i];
+//             count = 1;
+//         }
+//     }
+
+//     count = 0;
+//     for (int i = 0; i < N; i++) {
+//         if (arr[i] == candidate) count++;
+//     }
+
+//     if (count > N / 2) printf("%d\n", candidate);
+//     else printf("-1\n");
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
 int main() {
-    int N;
+    int N, candidate, count = 0;
     scanf("%d", &N);
-
     int arr[N];
+    for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+
     for (int i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    int candidate = arr[0], count = 1;
-
-    for (int i = 1; i < N; i++) {
-        if (arr[i] == candidate) count++;
-        else count--;
-        if (count == 0) {
-            candidate = arr[i];
-            count = 1;
-        }
+        if (count == 0) candidate = arr[i];
+        count += (arr[i] == candidate) ? 1 : -1;
     }
 
     count = 0;
-    for (int i = 0; i < N; i++) {
-        if (arr[i] == candidate) count++;
-    }
-
-    if (count > N / 2) printf("%d\n", candidate);
-    else printf("-1\n");
+    for (int i = 0; i < N; i++) if (arr[i] == candidate) count++;
+    printf(count > N / 2 ? "%d\n" : "-1\n", candidate);
 
     return 0;
 }
