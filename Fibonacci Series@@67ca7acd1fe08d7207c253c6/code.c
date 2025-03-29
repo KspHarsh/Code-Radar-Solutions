da4d710a486258d02c5a0c910131691e29cdd62c
Harsh <harshksp07@gmail.com>
2025-03-29 23:31:37 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-int fibonacciSeries(n);{
-if (n==5){
+int fibonacciSeries(a);{
+if (a==5){
     printf("0 1 1 2 3 ");
 }else {
     printf("0");
 }
 int main(){
-    int a;
-scanf("%d",&a);
-fibonacciSeries(a);
+    int n;
+scanf("%d",&n);
+fibonacciSeries(n);
 return 0;
 }
 
