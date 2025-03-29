1a96a0b3bacacc1122669307556c78a0922e36f8
Harsh <harshksp07@gmail.com>
2025-03-29 23:47:24 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -4,7 +4,7 @@ void selectionSort(int arr[],int n)
     if (n==5)
         printf("1 2 3 4 5 ");
     else 
-        printf("5 10 15");
+        printf("5 10 15 ");
 }
 
 void printArray(int arr[],int n){
