71878c16fa6aead37f2269271e8998a800fb9504
Harsh <harshksp07@gmail.com>
2025-03-30 16:56:20 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,50 +1,29 @@
-// // Your code here...
-// #include <stdio.h>
+// Your code here...
+#include <stdio.h>
 
-// int main() {
-//     int N;
-//     scanf("%d", &N);
+int main() {
+    int N;
+    scanf("%d", &N);
 
-//     int arr[N];
-//     for (int i = 0; i < N; i++) {
-//         scanf("%d", &arr[i]);
-//     }
+    int arr[N];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
 
-//     int isSorted = 1;
+    int isSorted = 1;
 
-//     for (int i = 1; i < N; i++) {
-//         if (arr[i] < arr[i - 1]) {
-//             isSorted = 0;
-//             break;
-//         }
-//     }
-
-//     if (isSorted) {
-//         printf("Sorted\n");
-//     } else {
-//         printf("Not Sorted\n");
-//     }
-
-//     return 0;
-// }
-
-
-void insertionSort(int arr[], int n) {
-        for (int i = 1; i < n; i++) {
-            for (int j = i; j > 0; j--) {  
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1]; 
-                    arr[j - 1] = temp;
-                } else {
-                    break;
-                }
-            }
+    for (int i = 1; i < N; i++) {
+        if (arr[i] < arr[i - 1]) {
+            isSorted = 0;
+            break;
         }
     }
-    void printArray(int arr[], int n) {
-        for (int i = 0; i < n; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
+
+    if (isSorted) {
+        printf("Sorted\n");
+    } else {
+        printf("Not Sorted\n");
     }
+
+    return 0;
+}
