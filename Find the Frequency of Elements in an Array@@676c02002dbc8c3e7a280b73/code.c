02e5838a9a7737844f5c4c5f6988af4e9ce2459f
Harsh <harshksp07@gmail.com>
2025-03-31 23:53:26 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,35 +1,16 @@
-#include<stdio.h>
-int main(){ 
-int n;
-scanf("%d",&n);
-int arr[n],i,j;
-for(i=0;i<n;i++)
-scanf("%d",&arr[i]);
-for(i=0;i<n;i++)
-if(arr[i]!=-1){
-    int c =1;
-    for(j=i+1;j<n;j++)
-    if(arr[i]==arr[j])
-    c++,
-    arr[j]=-1;
-    printf("%d %d\n",arr[i],c);
+#include <stdio.h>
+int main() {
+    int N; scanf("%d", &N);
+    int arr[N], i, j;
+    for (i = 0; i < N; i++) scanf("%d", &arr[i]);
+    for (i = 0; i < N; i++) if (arr[i] != -1) {
+        int c = 1;
+        for (j = i + 1; j < N; j++) if (arr[i] == arr[j]) c++, arr[j] = -1;
+        printf("%d %d\n", arr[i], c);
     }
-return 0;
-
+    return 0;
 }
 
-// #include <stdio.h>
-// int main() {
-//     int N; scanf("%d", &N);
-//     int arr[N], i, j;
-//     for (i = 0; i < N; i++) scanf("%d", &arr[i]);
-//     for (i = 0; i < N; i++) if (arr[i] != -1) {
-//         int c = 1;
-//         for (j = i + 1; j < N; j++) if (arr[i] == arr[j]) c++, arr[j] = -1;
-//         printf("%d %d\n", arr[i], c);
-//     }
-//     return 0;
-// }
 
 
 
