3e2c51c24b72183962033cdccc7a18ade03fa920
Harsh <harshksp07@gmail.com>
2025-04-01 00:26:38 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,37 +1,37 @@
-// void fibonacciSeries(int n) {
-//     int first = 0, second = 1, next;
+void fibonacciSeries(int n) {
+    int first = 0, second = 1, next;
 
-//     for (int i = 1; i <= n; i++) {
-//         if (i == 1) {
-//             printf("%d ", first);
-//             continue;
-//         }
-//         if (i == 2) {
-//             printf("%d ", second);
-//             continue;
-//         }
-//         next = first + second;
-//         printf("%d ", next);
-//         first = second;
-//         second = next;
-//     }
-//     printf("\n");
-
-
-void fibonacciSeries(int n){
-    int first=0,second=1,next;
-    for(int i=1;i<=n;i++){
-        if(i==1){
-            printf("%d ",first);
-            continue;
-        }if(i==2){
-            printf("%d ",second);
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) {
+            printf("%d ", first);
             continue;
         }
-        next =first+second;
-        printf("%d ",next);
-        first=second;
-        second=next;
+        if (i == 2) {
+            printf("%d ", second);
+            continue;
+        }
+        next = first + second;
+        printf("%d ", next);
+        first = second;
+        second = next;
     }
     printf("\n");
 }
+
+// void fibonacciSeries(int n){
+//     int first=0,second=1,next;
+//     for(int i=1;i<=n;i++){
+//         if(i==1){
+//             printf("%d ",first);
+//             continue;
+//         }if(i==2){
+//             printf("%d ",second);
+//             continue;
+//         }
+//         next =first+second;
+//         printf("%d ",next);
+//         first=second;
+//         second=next;
+//     }
+//     printf("\n");
+// }
